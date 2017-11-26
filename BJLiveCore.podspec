3c6995fa5b3a2fcff544ea8e59d2105e58b63ccc
iwill <minglq.9@gmail.com>
2017-11-26 20:33:55 +0800

    update

--- BJLiveCore.podspec
+++ BJLiveCore.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
     s.name          = 'BJLiveCore'
-    s.version       = '1.1.0'
+    s.version       = '1.3.0-alpha'
     s.summary       = 'BJLiveCore SDK.'
     s.description   = 'BJLiveCore SDK for iOS.'
 
@@ -25,22 +25,9 @@ Pod::Spec.new do |s|
         ss.ios.public_header_files  = 'frameworks/BJLiveCore.framework/Versions/A/Headers/**/*.h'
         ss.ios.resource             = 'frameworks/BJLiveCore.framework/Versions/A/Resources/**/*'
         ss.ios.vendored_frameworks  = 'frameworks/BJLiveCore.framework'
-        ss.frameworks               = ['CoreGraphics', 'Foundation', 'UIKit', 'WebKit']
+        ss.frameworks               = ['CoreGraphics', 'CoreTelephony', 'Foundation', 'UIKit', 'WebKit']
         ss.dependency 'BJLiveCore/BJHLMediaPlayer'
-        ss.dependency 'LogStat', '~> 0.7.3'
     end
-
-    # TODO: Assets.car
-    # s.subspec 'dynamic' do |ss|
-    #     ss.ios.preserve_paths       = 'frameworks/dylib/BJLiveCore.framework'
-    #     ss.ios.source_files         = 'frameworks/dylib/BJLiveCore.framework/Headers/**/*.h'
-    #     ss.ios.public_header_files  = 'frameworks/dylib/BJLiveCore.framework/Headers/**/*.h'
-    #     # ss.ios.resource             = 'frameworks/dylib/BJLiveCore.framework/Resources/**/*'
-    #     ss.ios.vendored_frameworks  = 'frameworks/dylib/BJLiveCore.framework'
-    #     ss.frameworks               = ['CoreGraphics', 'Foundation', 'UIKit', 'WebKit']
-    #     ss.dependency 'BJLiveCore/BJHLMediaPlayer'
-    #     ss.dependency 'LogStat', '0.7.3-dylib'
-    # end
 
     s.subspec 'BJHLMediaPlayer' do |ss|
         ss.xcconfig = {
@@ -52,15 +39,15 @@ Pod::Spec.new do |s|
         ss.libraries = ['icucore', 'c++', 'stdc++.6', 'z']
     end
 
-    s.dependency 'BJLiveBase', '~> 1.1.0'
-    s.dependency 'BJLiveBase/Base'
-    s.dependency 'BJLiveBase/Ext'
-    s.dependency 'BJLiveBase/Networking'
-    s.dependency 'BJLiveBase/WebImage/AFNetworking'
+    s.dependency "BJLiveBase", "~> 1.3.0-alpha"
+    s.dependency "BJLiveBase/Base"
+    s.dependency "BJLiveBase/Ext"
+    s.dependency "BJLiveBase/Networking"
+    s.dependency "BJLiveBase/PocketSocket/Client"
+    s.dependency "BJLiveBase/WebImage/AFNetworking"
+    s.dependency "BJLiveBase/YYModel"
 
-    s.dependency 'AFNetworking', '~> 3.0'
-    s.dependency 'PocketSocket'
-    s.dependency 'YYModel'
+    s.dependency "BJLLogStat", "~> 0.9.0-alpha"
 
     ## DEPRECATED
     s.dependency 'ReactiveObjC'
--- frameworks/BJLiveCore.framework/Versions/A/Headers/BJLConstants.h
+++ frameworks/BJLiveCore.framework/Versions/A/Headers/BJLConstants.h
@@ -14,12 +14,13 @@ NS_ASSUME_NONNULL_BEGIN
 /** WebSocket 请求超时时间 */
 extern const NSTimeInterval BJLWebSocketTimeoutInterval;
 
-/** 部署环境 */
-typedef NS_ENUM(NSInteger, BJLDeployType){
+/** 部署环境(内部使用) */
+typedef NS_ENUM(NSInteger, BJLDeployType) {
     /** 正式环境 */
     BJLDeployType_www,
     BJLDeployType_test,
     BJLDeployType_beta,
+    BJLDeployType_96kr,
     _BJLDeployType_count
 };
 
@@ -171,7 +172,7 @@ typedef NS_ENUM(NSInteger, BJLTextMaxLength) {
  whcel: https://help.aliyun.com/document_detail/32223.html?spm=5176.doc32228.6.969.IvVprX
  o:     https://help.aliyun.com/document_detail/32231.html?spm=5176.doc32223.6.977.fTzBHO
  ext:   https://help.aliyun.com/document_detail/32244.html?spm=5176.doc32223.2.2.fTzBHO
- */
+
 static inline NSString *BJLAliIMGURLParams_aspectScale(BOOL fill, NSInteger width, NSInteger height, NSInteger scale, NSString * _Nullable ext) {
     static const NSInteger aliIMGMaxSize = 4096;
     scale = MAX(1.0, scale <= 0 ? round([UIScreen mainScreen].scale) : scale);
@@ -188,15 +189,16 @@ static inline NSString *BJLAliIMGURLParams_aspectScale(BOOL fill, NSInteger widt
                         width, height, scale, fill];
     ext = ext.lowercaseString;
     return ext.length ? [params stringByAppendingPathExtension:ext] : params;
-}
+} */
 
-/*
+/**
  Ali image url params
- ?x-oss-process=image/resize,m_mfit,w_100,h_100,limit_1/auto-orient,1/format,jpg
+ x-oss-process=image/resize,m_mfit,w_100,h_100,limit_1/auto-orient,1/format,jpg
  /resize: m_lfit - aspect fit (default), m_mfit - aspect fill (no cut), w - width, h - height, limit_1 - no enlarge (default)
  /auto-orient: 1 - auto routate then resize (default?), 0 - resize without auto routate
  /format,jpg: jpg/png/webp/gif (lowercase!)
  https://help.aliyun.com/document_detail/44686.html?spm=5176.doc54739.3.2.kCK7Oh
+ */
 static inline NSString *BJLAliIMGURLParams_aspectScale(BOOL fill, NSInteger width, NSInteger height, NSInteger scale, NSString * _Nullable ext) {
     scale = MAX(1.0, scale <= 0 ? round([UIScreen mainScreen].scale) : scale);
     width *= scale;
@@ -211,22 +213,35 @@ static inline NSString *BJLAliIMGURLParams_aspectScale(BOOL fill, NSInteger widt
     width = MAX(1.0, width);
     height = MAX(1.0, height);
     ext = ext.lowercaseString;
-    return [NSString stringWithFormat:@
-            "?x-oss-process=image"
+    return [NSString stringWithFormat:@"image"
             "/resize,m_%@,w_%td,h_%td,limit_1"
             "/auto-orient,1"
             "/format,%@",
             fill ? @"mfit" : @"lfit", width, height,
             [@[@"jpg", @"png", @"webp", @"gif"] containsObject:ext] ? ext : @"jpg"];
-} */
+}
 
 static inline NSString *BJLAliIMGURLString_aspectScale(BOOL fill, NSInteger width, NSInteger height, NSInteger scale, NSString *urlString, NSString * _Nullable ext) {
-    NSUInteger atLocation = [urlString rangeOfString:@"@"].location;
-    if (atLocation != NSNotFound) {
-        urlString = [urlString substringToIndex:atLocation];
+    NSString * const key = @"x-oss-process";
+    
+    NSURLComponents *components = [NSURLComponents componentsWithString:urlString];
+    if (!components) {
+        return urlString;
     }
-    NSString *params = BJLAliIMGURLParams_aspectScale(fill, width, height, scale, ext ?: [urlString pathExtension]);
-    return urlString.length ? [urlString stringByAppendingString:params] : params;
+    
+    NSMutableArray *queryItems = [components.queryItems mutableCopy] ?: [NSMutableArray new];
+    for (NSURLQueryItem *queryItem in components.queryItems) {
+        if ([queryItem.name isEqualToString:key]) {
+            [queryItems removeObject:queryItem];
+        }
+    }
+    
+    NSString *value = BJLAliIMGURLParams_aspectScale(fill, width, height, scale, ext ?: [urlString pathExtension]);
+    NSURLQueryItem *queryItem = [NSURLQueryItem queryItemWithName:key value:value];
+    [queryItems addObject:queryItem];
+    components.queryItems = queryItems;
+    
+    return components.string;
 }
 
 static inline NSString *BJLAliIMG_aspectFill(CGSize size, CGFloat scale, NSString *urlString, NSString * _Nullable ext) {
